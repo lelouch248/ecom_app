4f43490f72df106b30ec7e20aecf927eecd0b804
lelouch248 <lelouch248@users.noreply.github.com>
2023-07-29 00:54:34 +0530

    added filter data

---
+++ lib/data/filters.dart
@@ -0,0 +1,6 @@
+var filters = {
+  "make": ["Apple", "OnePlus", "Samsung", "Xiomi", "Realme", "Vivo"],
+  "condition": ["Like New", "Excellent", "Good", "Fair", "Needs Repair"],
+  "storage": ["32 GB", "64 GB", "128 GB", "256 GB", "512 GB", "1 TB"],
+  "ram": ["4 GB", "6 GB", "8 GB", "12 GB", "16 GB", "32 GB", "64 GB", "128 GB"]
+};
